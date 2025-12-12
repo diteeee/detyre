fb4659896b08269d504595de634ff87dcf7dab4d
Dite Qela <149471713+diteeee@users.noreply.github.com>
2025-12-12 17:00:55 +0100

    fix problems

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 clone the repository:
+
 ```
 git clone https://github.com/diteeee/detyre.git
 cd detyre
 ```
 
+install dependencies:
+
+```
+npm install
+```
+
 build the container:
+
 ```
 docker-compose up --build
 ```
 
 the app should now be running on:
+
 ```
 http://localhost:3000
 ```
 
 to stop the app:
+
 ```
 docker-compose down
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import { MongoClient } from "mongodb";
+import express, { Request, Response } from "express";
+import { MongoClient, Db } from "mongodb";
 
 const app = express();
 app.use(express.json());
 
-const uri = process.env.MONGO_URI || "mongodb://localhost:27017/";
-const client = new MongoClient(uri);
+const uri: string = process.env.MONGO_URI || "mongodb://mongo:27017/detyre";
+const client: MongoClient = new MongoClient(uri);
 
-async function start() {
+async function start(): Promise<void> {
   try {
     await client.connect();
     console.log("Connected to MongoDB");
 
-    app.get("/", (_req, res) => {
+    app.get("/", (_req: Request, res: Response) => {
       res.send("Hello World!");
     });
     // e bona _req since it is never used to silence warnings (usually se kishim shkrujt qashtu)
