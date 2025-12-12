608d4effb3db3aa5df12974d14112d585dfcd749
Dite Qela <149471713+diteeee@users.noreply.github.com>
2025-12-12 14:38:01 +0100

    detyra

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const app = express();
+app.use(express.json());
+
+const uri = "mongodb://localhost:27017/";
+const client = new MongoClient(uri);
+
+async function start() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    app.get("/", (_req, res) => {
+      res.send("Hello World!");
+    });
+    // e bona _req since it is never used to silence warnings (usually se kishim shkrujt qashtu)
+
+    app.listen(3000, () => console.log("Server running on port 3000"));
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+}
+
+start();
